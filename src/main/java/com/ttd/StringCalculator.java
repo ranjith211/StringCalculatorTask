95a9dafd62e7993b20d6eb45211ed608aaf1763e
Ranjithkumar Alampally <ranjithkumar_alampally@epam.com>
2024-10-16 02:43:03 +0530

    add support till 2 numbers.

--- src/main/java/com/ttd/StringCalculator.java
+++ src/main/java/com/ttd/StringCalculator.java
@@ -1,7 +1,17 @@
 package com.ttd;
 
 public class StringCalculator {
-    public int add(String str) {
+    public int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+
+        String[] tokens = numbers.split(",");
+        if (tokens.length == 1) {
+            return Integer.parseInt(tokens[0]);
+        } else if (tokens.length == 2) {
+            return Integer.parseInt(tokens[0]) + Integer.parseInt(tokens[1]);
+        }
         return 0;
     }
 }
--- src/test/java/com/ttd/StringCalculatorTest.java
+++ src/test/java/com/ttd/StringCalculatorTest.java
@@ -6,9 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
 
+    StringCalculator calculator = new StringCalculator();
     @Test
     void add_emptyString_returnsZero() {
-        StringCalculator calculator = new StringCalculator();
+
         assertEquals(0, calculator.add(""));
+    }
+
+    @Test
+    void add_numbers_returnSum(){
+        assertEquals(1, calculator.add("1"));
+        assertEquals(6, calculator.add("1,5"));
     }
 }
